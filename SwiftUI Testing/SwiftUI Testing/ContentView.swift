e1b02e00d56e99abbbbb9dfd95add95553d75864
Wilma <kima_92@fastmail.com>
2020-05-03 20:31:15 -0400

    Added some textViews for the title and location

--- SwiftUI Testing/SwiftUI Testing/ContentView.swift
+++ SwiftUI Testing/SwiftUI Testing/ContentView.swift
@@ -10,10 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        
+        // Create a new Vertical Stack
+        // It can be created and used with or without parameters
+        VStack(alignment: .leading) {
+            
+            // To create a Text View (like labels)
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Text("California")
+                    .font(.subheadline)
+            }
+        }.padding()
+            
     }
 }
 
+// This tells the compiler what to show in the preview
+// to your right, and what not to show.
+// Also, this code is ignored when you push this app to
+// TestFlight or the App Store
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
--- SwiftUI Testing/SwiftUI Testing/SceneDelegate.swift
+++ SwiftUI Testing/SwiftUI Testing/SceneDelegate.swift
@@ -21,6 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view that provides the window contents.
         let contentView = ContentView()
+        //  ^^ This is what sets the initial View
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
