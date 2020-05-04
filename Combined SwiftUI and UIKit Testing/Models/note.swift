bf14834f4b5919aee8baa111a1d8efc980c27e63
Wilma <kima_92@fastmail.com>
2020-05-04 13:22:12 -0400

    Created a Note Model

---
+++ Combined SwiftUI and UIKit Testing/Models/note.swift
@@ -0,0 +1,13 @@
+//
+//  note.swift
+//  Combined SwiftUI and UIKit Testing
+//
+//  Created by macbook on 5/4/20.
+//  Copyright © 2020 WilmaRodz. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    var title: String?
+}
