ff2f5354539e8c767a5231345c87568aafee008b
Wilma <kima_92@fastmail.com>
2020-06-30 12:23:20 -0400

    Created a FirstViewController to ask for permission to the Camera before displaying the CameraViewController

---
+++ Camera Testing with Paul/View Controllers/FirstViewController.swift
@@ -0,0 +1,79 @@
+//
+//  FirstViewController.swift
+//  Camera Testing with Paul
+//
+//  Created by macbook on 6/30/20.
+//  Copyright © 2020 WilmaRodriguez. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class FirstViewController: UIViewController {
+    // This VC's only purpose is to get permission from the user
+    // to use the camera, before actually streaming from the camera
+    
+    // MARK: - DidLoad and DidAppear
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        showCameraIfGrantedPermission()
+    }
+    
+    // MARK: - Methods
+    
+    private func showCameraIfGrantedPermission() {
+        
+        // Check the status of authorization for video
+        let status = AVCaptureDevice.authorizationStatus(for: .video)
+        
+        // Next step base on current status
+        switch status {
+            
+        // 1st run and the user hasn't been asked to give permission
+        case .notDetermined:
+            requestPermission()
+            
+        // Parental Control limits access to video
+        case .restricted:
+            fatalError("Device has been restricted by parent or guardian")
+            
+        // 2nd+ run, the user didn't give permission on purpose or by accident, request again ( show how to enable
+        case .denied:
+            fatalError("Show them a link to settings so they can enable video")
+            
+        // 2nd+ run, user has given permission to use camera
+        case .authorized:
+            showCamera()
+            
+        @unknown default:
+            fatalError("Didn't handle a new state for AVCaptureDevice authorization")
+        }
+    }
+    
+    // Request Permission for the first time
+    private func requestPermission() {
+        
+        // Show Pop up requesting access to video from the camera for the first time (will never show this again for this app)
+        AVCaptureDevice.requestAccess(for: .video) { granted in
+            
+            
+            guard granted else {
+                fatalError("Tell user they need to give video permission")
+            }
+            
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
+    
+    private func showCamera() {
+        performSegue(withIdentifier: "ShowCamera", sender: self)
+    }
+}
