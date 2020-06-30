bbb6fed241e27fc8f8f8469fd09087d1dedfe75c
Wilma <kima_92@fastmail.com>
2020-06-30 13:22:44 -0400

    Can now display on screen what it's streamed from the camera

--- Camera Testing with Paul/View Controllers/CameraViewController.swift
+++ Camera Testing with Paul/View Controllers/CameraViewController.swift
@@ -7,20 +7,95 @@
 //
 
 import UIKit
+import AVFoundation
 
 class CameraViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    lazy private var captureSession = AVCaptureSession()
     
     // MARK: - Outlets
     
     @IBOutlet weak var cameraView: CameraPreviewView!
     @IBOutlet weak var recordButton: UIButton!
     
-    // MARK: - ViewDidLoad
+    // MARK: - DidLoad, DidAppear and DidDisappear
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Resize camera preview to fill the entire screen
         cameraView.videoPlayerView.videoGravity = .resizeAspectFill
+        
+        setUpCaptureSession()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Start streaming when this VC Appears
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        // Stop streaming when this VC desappears
+        captureSession.stopRunning()
+    }
+    
+    private func setUpCaptureSession() {
+        
+        captureSession.beginConfiguration()
+        
+        // 1.   Add inputs
+        let camera = bestCamera()
+        
+        // 2.   Video
+        
+        // If we can get the data from this camera, and add it as input to the camera session
+        guard let captureInput = try? AVCaptureDeviceInput(device: camera),
+            captureSession.canAddInput(captureInput) else {
+                fatalError("Can't create the input from the camera")
+        }
+        
+        // Add it to the session
+        captureSession.addInput(captureInput)
+        
+        // Choose the resolution
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        // ** 4K is 4 times the size of HD (you can try it later and see how it goes..
+        
+        
+        // 3.   Audio
+        
+        // 4.   Add Output
+        
+        // 5.   Recording
+        captureSession.commitConfiguration()
+        
+        // 6.   Live Preview
+        cameraView.session = captureSession // Starts streaming that video on  screen
+    }
+    
+    // Get the best camera option for this device
+    private func bestCamera() -> AVCaptureDevice {
+        
+        
+        if let ultraWideCamera = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return ultraWideCamera
+        }
+        
+        // All iPhones have a wide angle camera (front + back)
+        // If the previous fails, we can try thos one
+        if let wideCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return wideCamera
+        }
+        
+        fatalError("No cameras found on this device")
     }
     
     // MARK: - Actions
