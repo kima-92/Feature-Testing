6ece040ceb88db8b9f88d69b12a66810484991b9
Wilma <kima_92@fastmail.com>
2020-07-09 14:50:27 -0400

    ImageView with Constraints, it displays  it in the center of the screen

--- Programmatic Contraints Testing/View Controllers/ViewController.swift
+++ Programmatic Contraints Testing/View Controllers/ViewController.swift
@@ -33,7 +33,25 @@ class ViewController: UIViewController {
         // Give it a specific height and width
         // And specify where on screen to display it,
         // using x and y coordinates
-        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        //imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        
+        // Turn off AutoResizingContraints for this View
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // Center it Horizontally
+        // Using the VC'c view
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        // Center it Vertically
+        // Using the VC'c view
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        // Adjust it's Width
+        imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        // Adjust it's Height
+        imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        
+        // ** If you don't end it with ".isActive = true", it will
+        // create the constraint but not activate it (will NOT show on run time)
     }
 }
 
