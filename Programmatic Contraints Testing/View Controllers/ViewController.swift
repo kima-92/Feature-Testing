91f8004274dcee1a993d9f355d17323a55e2aded
Wilma <kima_92@fastmail.com>
2020-07-09 14:40:24 -0400

    Updated an ImageView's frame to specify where to place it on screen, plus specified it's width and height

--- Programmatic Contraints Testing/View Controllers/ViewController.swift
+++ Programmatic Contraints Testing/View Controllers/ViewController.swift
@@ -9,15 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - DidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addImagePawsView()
+    }
+    
+    // MARK: - Methods
+    
+    private func addImagePawsView() {
+        
+        // Add an ImageView to the VC
         let pawsImage = UIImage(named: "paws")
         let imageView = UIImageView(image: pawsImage)
         
         view.addSubview(imageView)
+        
+        // Modify this ImageView's Frame :
+        
+        // Give it a specific height and width
+        // And specify where on screen to display it,
+        // using x and y coordinates
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
     }
-    
 }
 
