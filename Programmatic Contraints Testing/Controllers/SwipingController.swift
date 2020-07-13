40fd62af331ef21b430927b90e5e04471339b105
Wilma <kima_92@fastmail.com>
2020-07-13 11:58:56 -0400

    Displaying 4 empty cells in the SwipingController CollectionView

--- Programmatic Contraints Testing/Controllers/SwipingController.swift
+++ Programmatic Contraints Testing/Controllers/SwipingController.swift
@@ -16,5 +16,18 @@ class SwipingController: UICollectionViewController {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .green
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+        
+        cell.backgroundColor = .red
+        return cell
     }
 }
