d988a7d98bafef0ad1fa662984232a5f760875f0
Wilma <kima_92@fastmail.com>
2020-07-23 13:45:42 -0400

    Added Two tests to FirstVCTests

--- Unit and UI Testing/Unit and UI TestingTests/FirstVCTests.swift
+++ Unit and UI Testing/Unit and UI TestingTests/FirstVCTests.swift
@@ -13,25 +13,40 @@ import XCTest
 // To test it, you need to import it with @testable
 
 class FirstVCTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testSum() {
+        let a = 11
+        let b = 32
+        
+        let firstVC = FirstViewController()
+        
+        let result = firstVC.add(a: a, b: b)
+        
+        // MARK: - Incorrect
+        
+        // This will run but it will NEVER Fail the test
+        if result == 42 {
+            // Test Passed
+        } else {
+            // Test Failed
         }
+        
+        // MARK: - Correct
+        
+        // This is almost the same as above ^^
+        // But it will actually Fail the test if it's not true
+        XCTAssert(result == 42)
     }
-
+    
+    func testSubtract() {
+        let a = 1
+        let b = 1
+        
+        let firstVC = FirstViewController()
+        
+        let result = firstVC.subtract(a: a, b: b)
+        
+        // Test with custome message
+        XCTAssert(result == 10, "testSubtrack did not give expected result")
+    }
 }
