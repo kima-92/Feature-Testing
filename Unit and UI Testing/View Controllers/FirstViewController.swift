9976b342be738726e906eed2462ca2b97adcfc69
Wilma <kima_92@fastmail.com>
2020-07-23 12:54:57 -0400

    Added some math functions to the FirstVC

--- Unit and UI Testing/View Controllers/FirstViewController.swift
+++ Unit and UI Testing/View Controllers/FirstViewController.swift
@@ -12,19 +12,21 @@ class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func add(a: Int, b: Int) -> Int {
+        return a + b
     }
-    */
-
+    
+    func subtract(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    func multiply(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    func divide(a: Int, b: Int) -> Int {
+        return a / b
+    }
 }
