975d6712b62dcf719775653e12bd38acb4e74aeb
Wilma <kima_92@fastmail.com>
2020-08-29 13:17:35 -0400

    Setting up the INTERN VC, the FirstVC

--- Delegate Pattern Testing/Helpers/Notes.swift
+++ Delegate Pattern Testing/Helpers/Notes.swift
@@ -8,7 +8,14 @@
 
 /*
  
- * You Need some setup in both the BOSS screen and the INTERN screen :
+ MARK: - Delegate Protocol
+ 
+    Its a one-to-one communication pattern
+ 
+    If your interested in a one-to-many communication pattern, look up Notifications and Observers
+ 
+ 
+ * You NEED some setup in both the BOSS screen and the INTERN screen:
  
     In the BOSS screen:
  
@@ -18,6 +25,17 @@
  
         3. A Delegate variable          -> This delegate will act as the INTERN, inside the BOSS VC
  
+    In the INTERN screen:
+ 
+        1. Set the INTERN as the BOSS's delegate -> Either in the prepare(for segue), or however you segued to the BOSS VC
+ 
+        2. Conform to the Protocol you created in the BOSS VC
+ 
+        3. Code these new functions     -> Code what you want to execute in the INTERN VC, based on what you got from the BOSS VC
  
  
+ 
+    * When the function is called inside the BOSS VC, it will excecute the code you wrote for this function in the INTERN VC.
+    * What ever parameters you gave this function inside the BOSS VC, you will have (the same parameters) in the INTERN VC when this function is called
+
  */
--- Delegate Pattern Testing/View Controllers/FirstViewController.swift
+++ Delegate Pattern Testing/View Controllers/FirstViewController.swift
@@ -30,14 +30,23 @@ class FirstViewController: UIViewController {
     @IBAction func chooseColorButtonTapped(_ sender: UIButton) {
     }
     
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        // Set this INTERN as the delegate in the BOSS VC
+        if segue.identifier == "ShowColorSelectionScreenSegue" {
+            guard let bossVC = segue.destination as? SecondViewController else { return }
+            
+            bossVC.selectionDelegate = self
+        }
     }
-    */
+}
 
+// Conforming to ColorSelectionDelegate
+extension FirstViewController: ColorSelectionDelegate {
+    
+    func didSelectColor(color: UIColor) {
+        view.backgroundColor = color
+    }
 }
