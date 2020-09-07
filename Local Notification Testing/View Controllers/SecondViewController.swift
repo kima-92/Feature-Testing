02a325595a61b0e73f3dd75a5b4191e111cbacbd
Wilma <kima_92@fastmail.com>
2020-09-07 16:26:13 -0400

    Creating New SecondVC to test updating a Notification

---
+++ Local Notification Testing/View Controllers/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  Local Notification Testing
+//
+//  Created by macbook on 9/7/20.
+//  Copyright © 2020 WilmaRodriguez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    // MARK: - Outlets
+
+    @IBOutlet weak var notification10secButton: UIButton!
+    @IBOutlet weak var resetNotificationButton: UIButton!
+    
+    // MARK: - DidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func notification10secButtonTapped(_ sender: UIButton) {
+    }
+    @IBAction func resetNotificationButtonTapped(_ sender: UIButton) {
+    }
+    
+    // MARK: - Methods
+}
