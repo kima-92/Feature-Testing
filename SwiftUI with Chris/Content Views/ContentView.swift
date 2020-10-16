6e476005937210792ab1884e1d386b0b2751c959
Wilma <kima_92@fastmail.com>
2020-10-16 12:03:47 -0400

    Init for SwiftUI with Chris

---
+++ SwiftUI with Chris/Content Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI with Chris
+//
+//  Created by macbook on 10/16/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
