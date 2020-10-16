f70c9fa55047bd1777efc8085085b7ea2caba444
Wilma <kima_92@fastmail.com>
2020-10-16 13:07:16 -0400

    Experimenting with VStack, Images and Spacers

--- SwiftUI with Chris/Content Views/ContentView.swift
+++ SwiftUI with Chris/Content Views/ContentView.swift
@@ -8,12 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // The body is expecting a SINGLE VIew,
+    // it will give you an error if there's more than one root View
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            
+            HStack(alignment: .top, spacing: 20, content: {
+                
+                Text("Hello")
+                Image("lemon")
+                    .resizable()
+                    .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
+                Button(action: {}
+                       
+                       // TODO
+                       ,
+                       label: {
+                        Image("doughnut")
+                            .resizable()
+                            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
+                       })
+            })
+            Spacer()
+        }
     }
 }
 
+
+// This code is for Preview porpuses -->
+// it will NOT affect the code at run time
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
