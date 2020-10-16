112d5109d0ac29c1b329e9ffbb7134c4f4c2d484
Wilma <kima_92@fastmail.com>
2020-10-16 13:49:06 -0400

    Adding Modifiers to a text

--- SwiftUI with Chris/Content Views/ModifiersContentView.swift
+++ SwiftUI with Chris/Content Views/ModifiersContentView.swift
@@ -14,11 +14,23 @@ struct ModifiersContentView: View {
      For the image to look rounded, put the cornerRadius first and THEN add the padding
      */
     var body: some View {
-        Image("pinkBuilding")
-            .resizable() // This tells SwiftUI it's ok to resize this image
-            .cornerRadius(30)
-            .padding() // Padding with no parameters gives the same padding around the entire image
-            .shadow(radius: 10)
+        
+        VStack {
+            
+            Image("pinkBuilding")
+                .resizable() // This tells SwiftUI it's ok to resize this image
+                .cornerRadius(30)
+                .padding() // Padding with no parameters gives the same padding around the entire image
+                .shadow(radius: 10) // 1st way to add a shadow
+            
+            Text("This pink building is totally amazing!!")
+                .fontWeight(.heavy)
+                .foregroundColor(.pink)
+                .font(.largeTitle)
+                .multilineTextAlignment(.center)
+                .shadow(color: .black, radius: 0.1, x: 2, y: 2) // 2nd way to add a shadow
+            
+        }
     }
 }
 
